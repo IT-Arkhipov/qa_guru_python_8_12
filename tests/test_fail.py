c8469ff6fb6957f200883d3d2aaf5af224066dc5
Andrei Arkhipov <it.arkhipov@gmail.com>
2023-10-31 19:36:14 +0300

    fail tests

---
+++ tests/test_fail.py
@@ -0,0 +1,15 @@
+class TestFail:
+    def test_fail1(self):
+        assert False
+
+    def test_fail2(self):
+        assert False
+
+    def test_fail3(self):
+        assert False
+
+    def test_fail4(self):
+        assert False
+
+    def test_fail5(self):
+        assert False
