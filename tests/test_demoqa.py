0d52ef2a0fde23668ad123b82c5ab97a6975c3ee
Andrei Arkhipov <it.arkhipov@gmail.com>
2023-10-31 23:39:13 +0300

    with PageObject

--- tests/test_demoqa.py
+++ tests/test_demoqa.py
@@ -1,45 +1,19 @@
-import os
-from selene import browser, have, command
-from selenium.webdriver.common.keys import Keys
 import allure
+
+from utils.form.registration_form import RegistrationForm
+from utils import users
 
 
 def test_demoqa_complete_form():
-    with allure.step('Открытие страницы с формой регистрации'):
-        browser.open("/automation-practice-form")
+    registration_form = RegistrationForm()
+    # GIVEN
+    with allure.step('Open registration form page'):
+        registration_form.open()
 
-    with allure.step('Заполнение формы'):
-        browser.element('#firstName').type('FirstName')
-        browser.element('#lastName').type('LastName')
+    # WHEN
+    with allure.step('Fill registration form'):
+        registration_form.fill_form(users.user)
 
-        browser.element('#userEmail').type('mymail@test.ru')
-        browser.element('#genterWrapper').all('label').element_by(have.text('Male')).click()
-        browser.element('#userNumber').type('9170770905')
-        browser.element('#dateOfBirthInput').send_keys(Keys.CONTROL + 'a').send_keys('11 Oct 2023').press_enter()
-        browser.element('#subjectsInput').type('Maths').press_enter()
-        browser.element('#hobbiesWrapper').perform(command.js.scroll_into_view)
-        browser.element('#hobbiesWrapper').all('label').element_by(have.text('Sports')).click()
-        browser.element('#uploadPicture').send_keys(os.path.abspath('img/sample.jpg'))
-        browser.element('#currentAddress').type('My current address')
-        browser.element('#state').perform(command.js.scroll_into_view).click()
-        browser.element('#react-select-3-input').type('NCR').press_enter()
-        browser.element('#city').click()
-        browser.element('#react-select-4-input').type('Delhi').press_enter()
-        browser.element('footer').execute_script('element.remove()')
-        browser.element('#submit').perform(command.js.click)
-
-    with allure.step('Проверка итоговой таблицы'):
-        browser.element('.table-responsive').all('td:nth-of-type(2)').should(have.texts(
-            'FirstName LastName',
-            'mymail@test.ru',
-            'Male',
-            '9170770905',
-            '11 October,2023',
-            'Maths',
-            'Sports',
-            'sample.jpg',
-            'My current address',
-            'NCR Delhi'))
-
-
-# browser.execute_script('document.querySelector("#fixedban").remove()')
+    # THEN
+    with allure.step('Assert filled form'):
+        registration_form.assert_registered_info(users.user)
