0bb8518b07bbe0fe5daf406eaa3676e2a15b371b
Andrei Arkhipov <it.arkhipov@gmail.com>
2023-11-01 00:18:38 +0300

    browser version

--- tests/conftest.py
+++ tests/conftest.py
@@ -27,6 +27,8 @@ def init_browser():
     browser.config.driver = driver
 
     browser.config.base_url = 'https://demoqa.com'
+    browser.config.window_width = 1280
+    browser.config.window_height = 1024
 
     yield browser
 
