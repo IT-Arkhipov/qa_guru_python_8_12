fc39e84a9761314c3a742be5d7598bdda2f4f1ae
Andrei Arkhipov <it.arkhipov@gmail.com>
2023-10-31 19:48:43 +0300

    test_pass.py

---
+++ tests/test_pass.py
@@ -0,0 +1,30 @@
+class TestPass:
+    def test_pass11(self):
+        assert True
+
+    def test_pass12(self):
+        assert True
+
+    def test_pass13(self):
+        assert True
+
+    def test_pass14(self):
+        assert True
+
+    def test_pass15(self):
+        assert True
+
+    def test_pass11(self):
+        assert True
+
+    def test_pass12(self):
+        assert True
+
+    def test_pass13(self):
+        assert True
+
+    def test_pass14(self):
+        assert True
+
+    def test_pass15(self):
+        assert True
